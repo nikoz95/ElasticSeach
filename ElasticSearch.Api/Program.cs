feece7d0aa6457ece04ba7a5d8530564316001be
Nikoloz Malidze <nmalidze@tbcbank.com.ge>
2025-12-08 18:36:07 +0400

    Enhance Docker configuration and streamline Swagger setup in the application

--- ElasticSearch.Api/Program.cs
+++ ElasticSearch.Api/Program.cs
@@ -48,17 +48,13 @@ using (var scope = app.Services.CreateScope())
 Console.WriteLine();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Elasticsearch API V1");
-        c.RoutePrefix = string.Empty; // Swagger at root
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Elasticsearch API V1");
+    c.RoutePrefix = string.Empty; // Swagger at root
+});
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
